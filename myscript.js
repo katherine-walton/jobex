c32d327b94ddb2609b82bfce68672329543400dd
katherine-walton <katherinefaithwalton@gmail.com>
2020-02-22 19:15:02 -0500

    mvp

---
+++ myscript.js
@@ -0,0 +1,28 @@
+// $("#body").val("testing123");
+
+// document.getElementById('body').value = 'testing123';
+// document.body.style.backgroundColor = '#FF0000';
+// document.getElementById('body').value.backgroundColor = '#FF0000';
+
+// console.log("something");
+
+// inputs = $('input');
+
+// let changeColor = document.getElementsByTagName('input');
+// console.log(changeColor);
+
+// define your username and password
+var myUsername = 'Katherine';
+// var myPassword = '...';
+
+// find the fiends in your login form
+var loginField = document.getElementById('input_firstName');
+// var passwordField = document.getElementById('...');
+
+// fill in your username and password
+loginField.value = myUsername;
+// passwordField.value = myPassword;
+
+// if you want, you can even automaticaly submit the login form
+// var loginForm = document.getElementById('...');
+// loginForm.submit();

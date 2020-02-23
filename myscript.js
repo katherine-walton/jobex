a4a55677ed7c1e7c6ea72471584495b76a54444a
katherine-walton <katherinefaithwalton@gmail.com>
2020-02-23 14:34:11 -0500

    added fields

--- myscript.js
+++ myscript.js
@@ -47,9 +47,42 @@ phoneField.value = phone;
 // console.log(emailField);
 
 var website = 'https://github.com/katherine-walton';
-var websiteField = document.getElementById('question_15686604');
+var websiteField = document.getElementById('job_application_answers_attributes_1_text_value');
 websiteField.value = website;
 
+https://www.linkedin.com/in/katherine-walton-f/
+
+var linkedin = 'https://github.com/katherine-walton';
+var linkedinField = document.getElementById('job_application_answers_attributes_0_text_value');
+linkedinField.value = linkedin;
+
+var hear = 'Through your website';
+var hearField = document.getElementById('job_application_answers_attributes_2_text_value');
+hearField.value = hear;
+
+var gender = '2';
+var genderField = document.getElementById('job_application_gender');
+genderField.value = gender;
+
+// var gender = '2';
+// var genderField = document.getElementById('job_application_gender');
+// genderField.value = gender;
+
+var veteran = '1';
+var veteranField = document.getElementById('job_application_veteran_status');
+veteranField.value = veteran;
+
+var disability = '2';
+var disabilityField = document.getElementById('job_application_disability_status');
+disabilityField.value = disability;
+
+// var location = 'Atlanta, GA';
+// var locationField = document.getElementById('job_application_answers_attributes_1_text_value');
+// locationField.value = location;
+
+// var start = '2021';
+// var startField = document.getElementById('start-date-year year background-field');
+// startField.value = start;
 
 
 
